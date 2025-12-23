0e56e9982e90bd576f17f0d5e00aba4141f53ce5
saeid ebrahimi <saeid.ebrahimi127@gmail.com>
2025-12-23 14:57:03 +0330

    fix: move CustomToaster position to top-center for better visibility

--- resources/js/components/custom-toaster.tsx
+++ resources/js/components/custom-toaster.tsx
@@ -1,5 +1,5 @@
 import { Toaster as SonnerToaster } from '@/components/ui/sonner';
 
 export const CustomToaster = () => {
-  return <SonnerToaster expand closeButton duration={5000} />;
+  return <SonnerToaster expand position="top-center" closeButton duration={5000} />;
 };
